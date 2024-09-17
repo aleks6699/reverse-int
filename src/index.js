d1b64dc541e46273145138d876b405b616b38407
Oleksandr <aleks6699@gmail.com>
2024-09-18 02:49:52 +0300

    feat:  created function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+  const str = Math.abs(n).toString();
   
-}
+  const reversedStr = str.split('').reverse().join('');
+  
+  const reversedNumber = parseInt(reversedStr, 10);
+  
+  return reversedNumber;
+};
